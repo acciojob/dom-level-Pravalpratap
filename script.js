e37e256b286b5cfec20441f060eb4257ad200a8e
Praval Pratap Singh <113175059+Pravalpratap@users.noreply.github.com>
2024-06-11 06:18:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let elment=document.getElementByID("level")
+
+let counter=1
+while(elment){
+	counter++;
+	elment=elment.parentNode
+	
+}
+alert('The level of the element is: ' + (counter-1));
